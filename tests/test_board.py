8f00fc83e190d13a8959de2182e79d69f0bd84b1
Roman Pomelov <roman.pomelov@skillbox.ru>
2021-01-17 15:37:17 +0300

    заготовка для тестов

---
+++ tests/test_board.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def test_initial_setup():
+    # 1. Поле правильного размера 8х8
+    # 2. Правильно выставились некоторые фигуры
+    pass
+
+
+def test_can_move():
+    pass
+
+
+def test_move():
+    pass
+
+
---
+++ tests/test_rook.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_cant_move_diagonal():
+    pass
+
+
+def test_cant_move_with_obstacles():
+    pass
+
+
+def test_icon():
+    pass
